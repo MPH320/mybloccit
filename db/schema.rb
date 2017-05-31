28a66cbd6e1910dffbb6ebf5c224a407810a7eea
Michelangelo Pereira <michelangelo.ph@gmail.com>
2017-05-31 03:45:39 +0000

    Rank added to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530231042) do
+ActiveRecord::Schema.define(version: 20170531033941) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170530231042) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
