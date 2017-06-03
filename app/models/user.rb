779d2b44ac6a5bbeb4f03f7e1bbf9ad0b81f0343
Michelangelo Pereira <michelangelo.ph@gmail.com>
2017-06-03 01:44:14 +0000

    User profiles added

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,4 +43,9 @@ class User < ActiveRecord::Base
    def favorite_forSP(sponsored_post)
      favorites.where(sponsored_post_id: sponsored_post.id).first
    end
+   
+   def avatar_url(size)
+     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+   end
 end
